6bc8d544974b01cf83a7e35e9b6f6a0b4f8ea69f
mnerv <24420859+mnerv@users.noreply.github.com>
2024-02-19 00:39:13 +0100

    CMake use miniaudio

--- midip.cpp
+++ midip.cpp
@@ -9,6 +9,10 @@
 
 #include "fmt/format.h"
 
+#ifdef MDP_MINIAUDIO
+#include "miniaudio.h"
+#endif
+
 // https://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html#BMA1_
 // http://www.somascape.org/midi/tech/mfile.html
 // https://www.ccarh.org/courses/253/handout/smf/
@@ -103,8 +107,6 @@ private:
     std::size_t m_sequence_size;
 };
 
-
-
 struct sysex_event {};
 
 struct midi_event {};
@@ -147,13 +149,24 @@ struct meta_tempo {
     std::uint8_t quarter_note;
 };
 
+enum class mtrk_event_type : std::uint32_t {
+    meta,
+    midi,
+    sysex
+};
+
+struct mtrk_event {
+    mtrk_event_type type;
+    std::size_t index;
+};
+
 struct mtrk {
     char const* type = "MTrk";
-    std::uint32_t length;
+    std::vector<mtrk_event> events{};
 
     auto str() const -> std::string {
-        return fmt::format("{} {{ length: {} }}",
-                           type,  length);
+        return fmt::format("{} {{ events: {} }}",
+                           type,  events.size());
     }
 
     static auto read(mdp::istrm& istrm) -> mtrk {
@@ -164,8 +177,8 @@ struct mtrk {
             throw std::runtime_error(fmt::format("Not a MIDI track: {}", std::string_view{type_buffer}));
         istrm.consume(sizeof type_buffer - 1);
 
-        istrm.peek_consume(reinterpret_cast<std::uint8_t*>(&chunk.length), sizeof chunk.length);
-        chunk.length = std::byteswap(chunk.length);
+        istrm.peek_consume(reinterpret_cast<std::uint8_t*>(&chunk.m_byte_size), sizeof chunk.m_byte_size);
+        chunk.m_byte_size = std::byteswap(chunk.m_byte_size);
         chunk.parse(istrm);
         return chunk;
     }
@@ -173,17 +186,20 @@ struct mtrk {
 private:
     auto parse(mdp::istrm& istrm) -> void {
         [[maybe_unused]]std::uint32_t dt = 0;
-        istrm.set_max_consume(length);
+        istrm.set_max_consume(m_byte_size);
         while (istrm.has_next()) {
             dt = istrm.read_vlq();
             if (istrm.is_sequence_match({0xFF, 0x00, 0x02})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Sequence Number\n");
             } else if (istrm.is_sequence_match({0xFF, 0x01})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Text Event\n");
             } else if (istrm.is_sequence_match({0xFF, 0x02})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Copyright Notice\n");
             } else if (istrm.is_sequence_match({0xFF, 0x03})) {
                 istrm.consume_last_sequence();
@@ -191,24 +207,31 @@ private:
                 std::vector<char> text_data{};
                 text_data.resize(len, 0x00);
                 istrm.peek_consume(reinterpret_cast<std::uint8_t*>(text_data.data()), len);
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Tack Name: {}\n", std::string_view{text_data});
             } else if (istrm.is_sequence_match({0xFF, 0x04})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Instrument Name\n");
             } else if (istrm.is_sequence_match({0xFF, 0x05})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Lyric\n");
             } else if (istrm.is_sequence_match({0xFF, 0x06})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Marker\n");
             } else if (istrm.is_sequence_match({0xFF, 0x07})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Cue Point\n");
             } else if (istrm.is_sequence_match({0xFF, 0x20, 0x01})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("MIDI Channel Prefix\n");
             } else if (istrm.is_sequence_match({0xFF, 0x2F, 0x00})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("End of Track\n");
             } else if (istrm.is_sequence_match({0xFF, 0x51, 0x03})) {
                 istrm.consume_last_sequence();
@@ -217,9 +240,11 @@ private:
                 std::uint32_t const tempo = (std::uint32_t(tempo_data[2]) << 0)
                                           | (std::uint32_t(tempo_data[1]) << 8)
                                           | (std::uint32_t(tempo_data[0]) << 16);
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Set Tempo {} ms per quarter note\n", tempo);
             } else if (istrm.is_sequence_match({0xFF, 0x54, 0x05})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("SMPTE Offset\n");
             } else if (istrm.is_sequence_match({0xFF, 0x58, 0x04})) {
                 istrm.consume_last_sequence();
@@ -227,33 +252,54 @@ private:
                 auto const denominator  = istrm.peek_consume();
                 auto const clicks       = istrm.peek_consume();
                 auto const quarter_note = istrm.peek_consume();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Time Signature {}/{}, {} MIDI clocks, {}\n", numerator, denominator, clicks, quarter_note);
             } else if (istrm.is_sequence_match({0xFF, 0x59, 0x02})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Key Signature");
             } else if (istrm.is_sequence_match({0xFF, 0x7F})) {
                 istrm.consume_last_sequence();
+                events.push_back({mtrk_event_type::meta, 0});
                 fmt::print("Sequencer Specific Meta-Event\n");
             } else if ((istrm.peek() & 0xF0) == 0xC0) {
                 std::uint8_t channel = istrm.peek_consume() & 0x0F;
                 std::uint8_t program = istrm.peek_consume() & 0x7F;
+                events.push_back({mtrk_event_type::midi, 0});
                 fmt::print("Program Change CH: {}, PR: {}\n", channel, program);
             } else if ((istrm.peek() & 0xF0) == 0x90) {
                 std::uint8_t channel = istrm.peek_consume() & 0x0F;
                 std::uint8_t key = istrm.peek_consume() & 0x7F;
                 std::uint8_t vel = istrm.peek_consume() & 0x7F;
+                events.push_back({mtrk_event_type::midi, 0});
                 fmt::print("{} Note On CH: {}, Key: {}, Vel {}\n", dt, channel, key, vel);
             } else if ((istrm.peek() & 0xF0) == 0x80) {
                 std::uint8_t channel = istrm.peek_consume() & 0x0F;
                 std::uint8_t key = istrm.peek_consume() & 0x7F;
                 std::uint8_t vel = istrm.peek_consume() & 0x7F;
+                events.push_back({mtrk_event_type::midi, 0});
                 fmt::print("{} Note Off CH: {}, Key: {}, Vel {}\n", dt, channel, key, vel);
+            } else if ((istrm.peek() & 0xF0) == 0xB0) {
+                std::uint8_t channel = istrm.peek_consume() & 0x0F;
+                std::uint8_t c = istrm.peek_consume() & 0x7F;
+                std::uint8_t v = istrm.peek_consume() & 0x7F;
+                events.push_back({mtrk_event_type::midi, 0});
+                fmt::print("{} Channel Mode Messages CH: {}, c: {}, v: {}\n", dt, channel, c, v);
+            } else if ((istrm.peek() & 0xF0) == 0xE0) {
+                std::uint8_t channel = istrm.peek_consume() & 0x0F;
+                std::uint8_t l = istrm.peek_consume() & 0x7F;
+                std::uint8_t m = istrm.peek_consume() & 0x7F;
+                events.push_back({mtrk_event_type::midi, 0});
+                fmt::print("{} Pitch Wheel Change CH: {}, l: {}, m: {}\n", dt, channel, l, m);
             } else {
                 fmt::print("Not Implemented: {:#04x}\n", istrm.peek_consume());
             }
-}
+        }
         istrm.reset_max_consume();
     }
+
+private:
+    std::uint32_t m_byte_size{};
 };
 
 struct mthd {
@@ -296,23 +342,66 @@ class midi {
 public:
     auto open(std::string const& midi_file) -> void {
         istrm strm(midi_file);
-        auto header = mthd::read(strm);
-        fmt::print("{}\n", header.str());
+        m_header = mthd::read(strm);
+        while (strm.has_next())
+            m_tracks.push_back(mtrk::read(strm));
 
-        while (strm.has_next()) {
-            auto track = mtrk::read(strm);
+        fmt::print("{}\n", m_header.str());
+        for (auto const& track : m_tracks)
             fmt::print("{}\n", track.str());
-        }
     }
 
 private:
+    mthd m_header{};
+    std::vector<mtrk> m_tracks{};
 };
 }
 
+#ifdef MDP_MINIAUDIO
+auto callback([[maybe_unused]]ma_device* device, [[maybe_unused]]void* output, [[maybe_unused]]void const* input, [[maybe_unused]]ma_uint32 frame_count) -> void {
+    ma_waveform* ptr = (ma_waveform*)device->pUserData;
+    ma_waveform_read_pcm_frames(ptr, output, frame_count, nullptr);
+}
+#endif
+
 static auto entry([[maybe_unused]]std::vector<std::string_view> const& args) -> void {
-    [[maybe_unused]]constexpr auto midi_file = "./Super Mario Bros. (Overworld Theme & MIDI).mid";
-    mdp::midi midi{};
-    midi.open(midi_file);
+    // waveform_config = ma_waveform_config_init(device.playback.format, device.playback.channels, device.sampleRate, ma_waveform_type_square, 0.1, 880.0);
+    // ma_waveform_init(&waveform_config, &waveform);
+    // std::this_thread::sleep_for(2s);
+
+#ifdef MDP_MINIAUDIO
+    ma_waveform_config waveform_config{};
+    ma_waveform waveform{};
+    ma_device device{};
+
+    ma_device_config config  = ma_device_config_init(ma_device_type_playback);
+    config.playback.format   = ma_format_f32;
+    config.playback.channels = 2;
+    config.sampleRate        = 48'000;
+    config.dataCallback      = callback;
+    config.pUserData         = &waveform;
+
+    if (ma_device_init(NULL, &config, &device) != MA_SUCCESS)
+        throw std::runtime_error("Failed to open playback device.");
+
+    fmt::print("Device Name: {}\n", device.playback.name);
+    if (ma_device_start(&device) != MA_SUCCESS) {
+        ma_device_uninit(&device);
+        throw std::runtime_error("Failed to start playback device.\n");
+    }
+
+    using namespace std::chrono_literals;
+    for (auto i = 0; i < 128; ++i) {
+        waveform_config = ma_waveform_config_init(device.playback.format, device.playback.channels, device.sampleRate, ma_waveform_type_square, 0.05, 440.0);
+        ma_waveform_init(&waveform_config, &waveform);
+        std::this_thread::sleep_for(50ms);
+        waveform_config = ma_waveform_config_init(device.playback.format, device.playback.channels, device.sampleRate, ma_waveform_type_square, 0.0, 440.0);
+        ma_waveform_init(&waveform_config, &waveform);
+        std::this_thread::sleep_for(50ms);
+    }
+
+    ma_device_uninit(&device);
+#endif
 }
 
 auto main([[maybe_unused]]int argc, [[maybe_unused]]char const* argv[]) -> int {
